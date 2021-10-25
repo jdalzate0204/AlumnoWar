3902b5d297c51ffa5a4c027a9a1ef87cad1be787
jdalzate0204 <james.alzate22@hotmail.com>
2021-10-25 16:59:13 -0500

    ExceptionWraper

--- src/main/java/co/edu/unicundi/alumnoswar/controller/AlumnoController.java
+++ src/main/java/co/edu/unicundi/alumnoswar/controller/AlumnoController.java
@@ -2,10 +2,7 @@ package co.edu.unicundi.alumnoswar.controller;
 
 import co.edu.unicundi.alumnosjar.service.IAlumnoService;
 import javax.ejb.EJB;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.MediaType;
 
 /**
  *
@@ -16,11 +13,4 @@ public class AlumnoController {
     
     @EJB
     private IAlumnoService service;
-    
-    @POST
-    @Path("/guardar")
-    @Consumes(MediaType.APPLICATION_JSON) //Enviar la información en JSON
-    public void insertar(){
-        System.out.println("Excelente");
-    }
 }
--- src/main/java/co/edu/unicundi/alumnoswar/exception/ExceptionHandler.java
+++ src/main/java/co/edu/unicundi/alumnoswar/exception/ExceptionHandler.java
@@ -1,5 +1,8 @@
 package co.edu.unicundi.alumnoswar.exception;
 
+import javax.ws.rs.NotAllowedException;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
@@ -11,19 +14,54 @@ import javax.ws.rs.ext.Provider;
  * @author acer
  */
 @Provider
-public class ExceptionHandler implements ExceptionMapper<Exception>{
-    @Context UriInfo uriInfo;
+public class ExceptionHandler implements ExceptionMapper<Exception> {
+    @Context private UriInfo uriInfo; 
     
     @Override
-    public Response toResponse(Exception ex) {
-        ex.printStackTrace();
+    public Response toResponse(Exception exception) {
+        exception.printStackTrace();
         ExceptionWraper ew;
         
-        if (ex instanceof ArrayIndexOutOfBoundsException) {
-            ew = new ExceptionWraper(Response.Status.BAD_REQUEST.getStatusCode(), Response.Status.BAD_REQUEST.getReasonPhrase(), ex.getMessage(), uriInfo.getPath());
-            return Response.status(Response.Status.BAD_REQUEST).entity(ew).build();
-        } else {
-            ew = new ExceptionWraper(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), Response.Status.INTERNAL_SERVER_ERROR.getReasonPhrase(), "", uriInfo.getPath());
+        if (exception instanceof IllegalArgumentException) { //400
+            ew = new ExceptionWraper(Response.Status.BAD_REQUEST.getStatusCode(), 
+                                      Response.Status.BAD_REQUEST.getReasonPhrase(), 
+                                      exception.getMessage(), 
+                                      uriInfo.getPath());
+            return Response.status(Response.Status.BAD_REQUEST).entity(ew).build();  
+        } 
+        else if (exception instanceof NotFoundException | exception instanceof IllegalStateException) { //404
+            ew = new ExceptionWraper(Response.Status.NOT_FOUND.getStatusCode(), 
+                                      Response.Status.NOT_FOUND.getReasonPhrase(), 
+                                      exception.getMessage(),
+                                      uriInfo.getPath());
+            return Response.status(Response.Status.NOT_FOUND).entity(ew).build();
+        } 
+        else if (exception instanceof NotAllowedException) { //405
+            ew = new ExceptionWraper(Response.Status.METHOD_NOT_ALLOWED.getStatusCode(), 
+                                      Response.Status.METHOD_NOT_ALLOWED.getReasonPhrase(), 
+                                      "Operación de petición incorrecta", 
+                                      uriInfo.getPath());
+            return Response.status(Response.Status.METHOD_NOT_ALLOWED).entity(ew).build();
+        } 
+        else if (exception instanceof CloneNotSupportedException) { //409
+            ew = new ExceptionWraper(Response.Status.CONFLICT.getStatusCode(), 
+                                      Response.Status.CONFLICT.getReasonPhrase(), 
+                                      exception.getMessage(), 
+                                      uriInfo.getPath());
+            return Response.status(Response.Status.CONFLICT).entity(ew).build();
+        } 
+        else if (exception instanceof WebApplicationException) { //415
+            ew = new ExceptionWraper(Response.Status.UNSUPPORTED_MEDIA_TYPE.getStatusCode(), 
+                                      Response.Status.UNSUPPORTED_MEDIA_TYPE.getReasonPhrase(), 
+                                      "Formato mal formado", 
+                                      uriInfo.getPath());
+            return Response.status(Response.Status.UNSUPPORTED_MEDIA_TYPE).entity(ew).build();
+        }
+        else { //500
+            ew = new ExceptionWraper(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), 
+                                      Response.Status.INTERNAL_SERVER_ERROR.getReasonPhrase(), 
+                                      "Error interno", 
+                                      uriInfo.getPath());
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ew).build();
         }
     }
