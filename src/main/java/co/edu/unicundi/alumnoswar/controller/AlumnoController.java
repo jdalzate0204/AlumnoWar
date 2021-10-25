24a43430036875f54b6e615a88466e37a9141f7c
jdalzate0204 <james.alzate22@hotmail.com>
2021-10-25 18:10:33 -0500

    POST y GET

--- src/main/java/co/edu/unicundi/alumnoswar/controller/AlumnoController.java
+++ src/main/java/co/edu/unicundi/alumnoswar/controller/AlumnoController.java
@@ -2,20 +2,9 @@ package co.edu.unicundi.alumnoswar.controller;
 
 import co.edu.unicundi.alumnosjar.entity.Alumno;
 import co.edu.unicundi.alumnosjar.service.IAlumnoService;
-import java.util.List;
 import javax.ejb.EJB;
-<<<<<<< HEAD
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-=======
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
->>>>>>> 9b34886f60438c40188537fbe979a8052c0a152a
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
 
 /**
  *
@@ -27,7 +16,6 @@ public class AlumnoController {
     @EJB
     private IAlumnoService service;
     
-<<<<<<< HEAD
     @GET
     @Path("/obtener/{id}")
     @Produces(MediaType.APPLICATION_JSON)
@@ -35,7 +23,7 @@ public class AlumnoController {
         Alumno alumno = service.listarPorId(id);
         return Response.status(Response.Status.OK).entity(alumno).build();
     }
-=======
+
     @POST
     @Path("/insertar")
     @Consumes(MediaType.APPLICATION_JSON) //Enviar la información en JSON
@@ -44,6 +32,4 @@ public class AlumnoController {
         System.out.println("Registrado correctamente");
         return Response.status(Response.Status.CREATED).build();
     }
-    
->>>>>>> 9b34886f60438c40188537fbe979a8052c0a152a
 }
