9b34886f60438c40188537fbe979a8052c0a152a
1Alejandra3 <palejandraguzman@ucundinamarca.edu.co>
2021-10-25 17:45:43 -0500

    guardar

--- src/main/java/co/edu/unicundi/alumnoswar/controller/AlumnoController.java
+++ src/main/java/co/edu/unicundi/alumnoswar/controller/AlumnoController.java
@@ -1,8 +1,14 @@
 package co.edu.unicundi.alumnoswar.controller;
 
+import co.edu.unicundi.alumnosjar.entity.Alumno;
 import co.edu.unicundi.alumnosjar.service.IAlumnoService;
+import java.util.List;
 import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -13,4 +19,14 @@ public class AlumnoController {
     
     @EJB
     private IAlumnoService service;
+    
+    @POST
+    @Path("/insertar")
+    @Consumes(MediaType.APPLICATION_JSON) //Enviar la información en JSON
+    public Response insertar(Alumno alumno){
+        service.guardar(alumno);
+        System.out.println("Registrado correctamente");
+        return Response.status(Response.Status.CREATED).build();
+    }
+    
 }
