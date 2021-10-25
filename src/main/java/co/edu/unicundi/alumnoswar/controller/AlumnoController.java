b287cd28488a708a2bcbfbdf17898111a8a5125b
jdalzate0204 <james.alzate22@hotmail.com>
2021-10-25 17:45:50 -0500

    editar

--- src/main/java/co/edu/unicundi/alumnoswar/controller/AlumnoController.java
+++ src/main/java/co/edu/unicundi/alumnoswar/controller/AlumnoController.java
@@ -1,8 +1,14 @@
 package co.edu.unicundi.alumnoswar.controller;
 
+import co.edu.unicundi.alumnosjar.entity.Alumno;
 import co.edu.unicundi.alumnosjar.service.IAlumnoService;
 import javax.ejb.EJB;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -13,4 +19,12 @@ public class AlumnoController {
     
     @EJB
     private IAlumnoService service;
+    
+    @GET
+    @Path("/obtener/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response obtenerPorId(@PathParam("id") Integer id) {
+        Alumno alumno = service.listarPorId(id);
+        return Response.status(Response.Status.OK).entity(alumno).build();
+    }
 }
