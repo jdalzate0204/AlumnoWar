8af706e75e8a781d4e576f57da15add7fe08b32b
1Alejandra3 <palejandraguzman@ucundinamarca.edu.co>
2021-10-25 20:47:51 -0500

    Get lista completa

--- src/main/java/co/edu/unicundi/alumnoswar/controller/AlumnoController.java
+++ src/main/java/co/edu/unicundi/alumnoswar/controller/AlumnoController.java
@@ -2,6 +2,7 @@ package co.edu.unicundi.alumnoswar.controller;
 
 import co.edu.unicundi.alumnosjar.entity.Alumno;
 import co.edu.unicundi.alumnosjar.service.IAlumnoService;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
@@ -23,11 +24,19 @@ public class AlumnoController {
         Alumno alumno = service.listarPorId(id);
         return Response.status(Response.Status.OK).entity(alumno).build();
     }
+    
+    @GET
+    @Path("/obtener")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response obtener(){
+        List<Alumno> listaAlumno=service.listar();
+        return Response.status(Response.Status.OK).entity(listaAlumno).build();
+    }
 
     @POST
     @Path("/insertar")
     @Consumes(MediaType.APPLICATION_JSON) //Enviar la información en JSON
-    public Response insertar(Alumno alumno){
+    public Response insertar(Alumno alumno) throws CloneNotSupportedException{
         service.guardar(alumno);
         System.out.println("Registrado correctamente");
         return Response.status(Response.Status.CREATED).build();
