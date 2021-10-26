f7ca5bc47b78c3fa12de2925485ef9d5f65f9077
jdalzate0204 <james.alzate22@hotmail.com>
2021-10-25 20:52:48 -0500

    necesito obtener

--- src/main/java/co/edu/unicundi/alumnoswar/controller/AlumnoController.java
+++ src/main/java/co/edu/unicundi/alumnoswar/controller/AlumnoController.java
@@ -2,7 +2,9 @@ package co.edu.unicundi.alumnoswar.controller;
 
 import co.edu.unicundi.alumnosjar.entity.Alumno;
 import co.edu.unicundi.alumnosjar.service.IAlumnoService;
+import java.util.HashMap;
 import javax.ejb.EJB;
+import javax.validation.ConstraintViolation;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 
@@ -26,10 +28,27 @@ public class AlumnoController {
 
     @POST
     @Path("/insertar")
-    @Consumes(MediaType.APPLICATION_JSON) //Enviar la información en JSON
-    public Response insertar(Alumno alumno){
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response insertar(Alumno alumno) {
         service.guardar(alumno);
         System.out.println("Registrado correctamente");
         return Response.status(Response.Status.CREATED).build();
+    }
+    
+    @PUT
+    @Path("/editar")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response editar(Alumno alumno) throws CloneNotSupportedException{
+        HashMap<String, String> errores = new HashMap();
+
+        for (ConstraintViolation error: alumno.validar())
+            errores.put(error.getPropertyPath().toString(), error.getMessage());
+
+        if (errores.size() > 0)
+            throw new IllegalArgumentException(errores.toString());
+        else {
+            service.editar(alumno);
+            return Response.status(Response.Status.OK).build();
+        }
     }
 }
